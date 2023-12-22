8364d0e69b2181056c67f9a2e20804f6950515eb
AndreasP7 <papakitsosandreas@gmail.com>
2023-12-22 20:27:06 +0200

    Added Door

--- Assets/Scripts/General/Door.cs
+++ Assets/Scripts/General/Door.cs
@@ -3,51 +3,90 @@ using System.Collections.Generic;
 using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
+
 
 public class Door : MonoBehaviour
 {
-    [SerializeField] private Text text;
-    [SerializeField] GameObject trigger;
-    [SerializeField] string doorOpen;
-    [SerializeField] string doorClose;
-    [SerializeField] private Animator animator;
-
-
-    private bool action = false;
     private bool triggered = false;
-    public bool isOpen = false;
+    [SerializeField] private float rotationSpeed = 10f;
+    [SerializeField] private float rotationAngle = 90f;
+    [SerializeField] private bool isOpen = false;
+    float direction =1;
+    bool isRotating = false;
+    Vector3 v;
+    Vector3 targetDirection;
+
+    GameObject player;
+
+    private void Update()
+    {
+        if (triggered && Input.GetKeyDown(KeyCode.E) && !isRotating)
+        {
+
+            
+           
+
+
+            if (isOpen)
+            {
+                v = Vector3.down;
+
+            }
+            else
+            {
+                v = Vector3.up;
+                //targetDirection = player.transform.position - transform.position;
+                targetDirection = player.transform.InverseTransformPoint(transform.position);
+                Debug.Log(targetDirection);
+                direction = Math.Sign(targetDirection.x);
+            }
+            
+            StartCoroutine(RotateDoor());
+            
+        }
+    }
+
+    IEnumerator RotateDoor()
+    {
+        isRotating = true;
+
+        float elapsedTime = 0f;
+
+        while (elapsedTime < rotationAngle / rotationSpeed)
+        {
+            float step = rotationSpeed * Time.deltaTime;
+            transform.Rotate(v * step * direction);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        isRotating = false;
+        isOpen = !isOpen;
+
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+         player = other.gameObject;
+         Debug.Log("trigger enter");
+         triggered = true;
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+         Debug.Log("trigger exit");
+         triggered = false;
+
+       
+    }
+
+
 
     
-
-
-
-    void OnTriggerEnter(Collider collision)
-    {
-        if (collision.transform.tag == "Player")
-        {
-            text.gameObject.SetActive(true);
-            action = true;
-        }
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            if (Input.GetButtonDown("Use"))
-            {
-                ToggleDoor();
-            }
-        }
-    }
-
-    void ToggleDoor()
-    {
-        if (isOpen)
-        {
-
-        }
-    }
 
    
 }
