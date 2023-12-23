8862d1d86732c54815cfd6c437e225d47083acea
Pavlos Ts <tsikrikaspavlos@gmail.com>
2023-12-23 14:32:10 +0200

    Inventory Script added

---
+++ Assets/Scripts/General/Inventory/InventoryManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager Instance;
+    public List<Item> Items = new List<Item>();
+
+    public Transform ItemContent;
+    public GameObject InventoryItem;
+
+    private void Awake()
+    {
+        Instance = this; 
+    }
+
+    public void Add(Item item)
+    { 
+        Items.Add(item);    
+    }
+
+    public void Remove(Item item) 
+    { 
+        Items.Remove(item);
+    }
+
+    public void ListItems()
+    {
+        foreach (Transform item in ItemContent)
+        {
+            Destroy(item.gameObject);
+        }    
+
+        foreach (var item in Items)
+        {
+            GameObject obj = Instantiate(InventoryItem, ItemContent);
+            var itemName = obj.transform.Find("ItemName").GetComponent<TMPro.TextMeshProUGUI>();
+            var itemIcon = obj.transform.Find("ItemIcon").GetComponent<UnityEngine.UI.Image>();
+            
+            itemName.text = item.itemName;
+            itemIcon.sprite = item.icon;
+        }
+    }
+}
--- Assets/Scripts/General/Inventory/ItemController.cs
+++ Assets/Scripts/General/Inventory/ItemController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Inventory : MonoBehaviour
+public class ItemController : MonoBehaviour
 {
-    
+    public Item Item;
 }
